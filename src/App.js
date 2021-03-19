7e00d39dc3f09f93aa76a1835b2d9f558e6d8cf3
angelvillago <angelvillago@gmail.com>
2021-03-19 19:58:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import saludo from './saludo';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <saludo/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/saludo.js
@@ -0,0 +1,5 @@
+export default function saludo () {
+    return ( 
+        <div> Hola mundo <div/>
+    );
+}
